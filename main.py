1071d9251ca216258c427993252fb5ce06ab402a
RadarChallenger <radara350@gmail.com>
2020-12-04 00:41:16 +1100

    First commit. Move file working

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+import shutil
+
+os.rename("C:/Users/radar/Github/testfile1/Randomtext.txt", "C:/Users/radar/Github/testfile2/Randomtext.txt")
+#shutil.move("C:/Users/radar/Github/testfile1/Randomtext.txt", "C:/Users/radar/Github/testfile2/Randomtext.txt")
+#os.replace("C:/Users/radar/Github/testfile1/Randomtext.txt", "C:/Users/radar/Github/testfile2/Randomtext.txt")
